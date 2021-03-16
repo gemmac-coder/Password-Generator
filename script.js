d40e48550718fc62c80ef4f211dd915722e8f34f
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-03-16 17:22:33 +0000

    adding character arrays, adding password length prompts and if statements

--- script.js
+++ script.js
@@ -1,13 +1,126 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var lowercaseArray = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+var uppercaseArray = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+var numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var specialCharactersArray =  [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
 
-  passwordText.value = password;
+// Declare a new function on line 3
+function generatePassword() {
+  // Asking the user their desired password length
+  var PasswordLength = prompt(
+    "How many characters would you like in your password? Passwords must be at least 8 characters, but no more than 128 characters."
+  );
+  // Converts user-defined password length string into a number
+  PasswordLength = Number.parseInt(passwordLength);
+
+//Checking to see if password length is valid
+
+// If the user password length is less than 8 characters, an alert shows and an empty string is returned.
+  if (PasswordLength < 8) {
+    alert("Your password must have more than 7 characters");
+  }
+  // If the user password length is more than 128 characters, an alert shows and an empty string is returned.
+  if (PasswordLength < 128) {
+    alert("Your password can have no more than 128 characters");
+  }
+  // If the user's password length is between 8 and 128 characters, this requested password length is logged to the console. 
+  if(passwordLength >=8 && passwordLength <=128) {
+console.log(userPasswordLength);
+
+//Asking the user about the character types they would like to include in their password.
+var lowercaseCharacters = confirm("Would you like to include lower case characters in your password?");
+var uppercaseCharacters = confirm("Would you like to include upper case characters in your password?");
+var numericCharacters = confirm("Would you like to include numeric characters in your password?");
+var specialCharactersArray = confirm("Would you like to include special characters in your password?");
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+  }
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
